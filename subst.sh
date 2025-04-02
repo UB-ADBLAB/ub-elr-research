182d06a6bc3dd4a07ada05d5d09a49bb761b0b76
Zhuoyue Zhao <zzhao35@buffalo.edu>
2025-04-02 16:31:24 -0400

    update

--- subst.sh
+++ subst.sh
@@ -32,49 +32,58 @@ for i in contents/prjs/*; do
     echo "Processing semester $SEMESTER ($S)"
     rm -f "out.tmp/${S}_prjids.txt"
     rm -f "out.tmp/${S}_prj_list.txt"
-    for j in contents/prjs/$S/*; do
-        echo "Processing project $j"
-        ID="$(grep '^ID=' $j | head -n 1 | sed 's/^ID=\([0-9]*\).*$/\1/')"
-        TITLE="$(grep '^TITLE=' $j | head -n 1 | sed 's/^TITLE=[ \t]*//' | sed 's/\//\\\//g')"
-        KEYWORDS="$(grep '^KEYWORDS=' $j | head -n 1 | sed 's/^KEYWORDS=[ \t]*//' | sed 's/\//\\\//g')"
-        MENTORS="$(grep '^MENTORS=' $j | head -n 1 | sed 's/^MENTORS=[ \t]*//' | sed 's/\//\\\//g')"
-        echo "$ID" >> out.tmp/${S}_prjids.txt
-        echo "$ID - $TITLE (mentored by $MENTORS)" | sed 's/\\//g' | sed 's/<[^<>]*>//g' >> out.tmp/${S}_prj_list.txt
-        
-        DESC="$(sed -n '
-            /^DESC_START/,/^DESC_END/ {
-                /^\(DESC_START\|DESC_END\)/d
-                p
-            }
-        ' $j)"
-        DETAILS="$(sed -n '
-            /^DESC_MORE_DETAILS_START/,/^DESC_MORE_DETAILS_END/ {
-                /^\(DESC_MORE_DETAILS_START\|DESC_MORE_DETAILS_END\)/d
-                p
-            }
-        ' $j)"
-         
-        echo "$DESC" > "out.tmp/${S}_${ID}_desc.txt"
-        echo "$DETAILS" > "out.tmp/${S}_${ID}_details.txt"
-        
-        cd out.tmp
-        sed -e "s/<!-- @PRJ_ID@ -->/$ID/" \
-            -e "s/<!-- @PRJ_TITLE@ -->/$TITLE/" \
-            -e "s/<!-- @MENTORS@ -->/$MENTORS/" \
-            -e "s/<!-- @KEYWORD@ -->/$KEYWORDS/" \
-            -e "/<!-- @SHORTDESC@ -->/{
-                r ${S}_${ID}_desc.txt
-                d
-            }" \
-            -e "/<!-- @LONGDESC@ -->/{
-                r ${S}_${ID}_details.txt
-                d
-            }" \
-            ../templates/prj_item.html > "${S}_${ID}.html"
-        cd ..
-    done
-    cat `sort -n "out.tmp/${S}_prjids.txt" | sed -e "s/^/out.tmp\/${S}_/" -e "s/$/.html/"` \
-        > "out.tmp/${S}_prj_cat.html"
+    PRJLIST="`find "contents/prjs/$S" -name '*.txt' | sort`"
+    if [ -n "$PRJLIST" ]; then
+        for j in $PRJLIST; do
+            echo "Processing project $j"
+            ID="$(grep '^ID=' $j | head -n 1 | sed 's/^ID=\([0-9]*\).*$/\1/')"
+            TITLE="$(grep '^TITLE=' $j | head -n 1 | sed 's/^TITLE=[ \t]*//' | sed 's/\//\\\//g')"
+            KEYWORDS="$(grep '^KEYWORDS=' $j | head -n 1 | sed 's/^KEYWORDS=[ \t]*//' | sed 's/\//\\\//g')"
+            MENTORS="$(grep '^MENTORS=' $j | head -n 1 | sed 's/^MENTORS=[ \t]*//' | sed 's/\//\\\//g')"
+            CAPACITY="$(grep '^CAPACITY=' $j | head -n 1 | sed 's/^CAPACITY=[ \t]*//' | sed 's/\//\\\//g' )"
+            echo "$ID" >> out.tmp/${S}_prjids.txt
+            echo "$ID - $TITLE (mentored by $MENTORS)" | sed 's/\\//g' | sed 's/<[^<>]*>//g' >> out.tmp/${S}_prj_list.txt
+            
+            DESC="$(sed -n '
+                /^DESC_START/,/^DESC_END/ {
+                    /^\(DESC_START\|DESC_END\)/d
+                    p
+                }
+            ' $j)"
+            DETAILS="$(sed -n '
+                /^DESC_MORE_DETAILS_START/,/^DESC_MORE_DETAILS_END/ {
+                    /^\(DESC_MORE_DETAILS_START\|DESC_MORE_DETAILS_END\)/d
+                    p
+                }
+            ' $j)"
+             
+            echo "$DESC" > "out.tmp/${S}_${ID}_desc.txt"
+            echo "$DETAILS" > "out.tmp/${S}_${ID}_details.txt"
+            
+            cd out.tmp
+            sed -e "s/<!-- @PRJ_ID@ -->/$ID/" \
+                -e "s/<!-- @PRJ_TITLE@ -->/$TITLE/" \
+                -e "s/<!-- @MENTORS@ -->/$MENTORS/" \
+                -e "s/<!-- @KEYWORD@ -->/$KEYWORDS/" \
+                -e "s/<!-- @CAPACITY@ -->/$CAPACITY/" \
+                -e "/<!-- @SHORTDESC@ -->/{
+                    r ${S}_${ID}_desc.txt
+                    d
+                }" \
+                -e "/<!-- @LONGDESC@ -->/{
+                    r ${S}_${ID}_details.txt
+                    d
+                }" \
+                ../templates/prj_item.html > "${S}_${ID}.html"
+            cd ..
+        done
+        cat `sort -n "out.tmp/${S}_prjids.txt" | sed -e "s/^/out.tmp\/${S}_/" -e "s/$/.html/"` \
+            > "out.tmp/${S}_prj_cat.html"
+    else
+        touch "out.tmp/${S}_prjids.txt"
+        touch "out.tmp/${S}_prj_list.txt"
+        touch "out.tmp/${S}_prj_cat.html"
+    fi
     sed -e "$REGULAR_SUBST" \
         -e "/<!-- @PRJS@ -->/{
             r out.tmp/${S}_prj_cat.html
